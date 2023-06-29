df3d70c4e2332ef4e77c4426cbc12e0411f07a70
okok111 <1for.rails@gmail.com>
2023-06-29 11:07:46 +0900

    room messageのモデルとコントローラ作成、チャンネル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
   devise_for :users
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
